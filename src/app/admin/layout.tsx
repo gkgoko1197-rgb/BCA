481535f28585ddac65ae2d59a647ffb7136a3497
Dinesh <dineshgopi0505@gmail.com>
2026-02-03 08:24:43 +0000

    MAKE THIS AS FIXED NOT MOVE WHEN SCROLE (_for element div_)

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -59,7 +59,7 @@ export default function AdminLayout({
   }
 
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+    <div className="grid h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <div className="hidden border-r bg-muted/40 md:block">
         <div className="flex h-full max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
@@ -121,7 +121,7 @@ export default function AdminLayout({
             </h1>
           </div>
         </header>
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-background">
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-background overflow-auto">
           {children}
         </main>
       </div>
